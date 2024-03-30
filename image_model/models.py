d4df4ac9210ef1e5ad8c484d46194a293df43106
harikris001 <hk5983835@gmail.com>
2024-03-30 21:03:23 +0530

    chnaged database

--- image_model/models.py
+++ image_model/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    dataset = models.FileField(upload_to="uploads/Image_datasets")
+    dataset = models.FileField()
     pjt_type = models.CharField(max_length=20)
