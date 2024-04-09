fe59736830bd384dc0b87cb81ece5b27512fdf24
harikris001 <hk5983835@gmail.com>
2024-04-09 11:39:43 +0530

    detection models

---
+++ image_model/detection_models.py
@@ -0,0 +1,7 @@
+import torchvision
+
+def fasterRCNN(output_classes: int):
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, output_classes)
+    return model
