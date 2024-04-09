f36fb783da6140f599327dbcf57300e5e921e0fe
harikris001 <hk5983835@gmail.com>
2024-04-09 11:39:31 +0530

    detection backend

---
+++ image_model/detectection_backend.py
@@ -0,0 +1,77 @@
+import os
+from PIL import Image
+
+import torch
+from bs4 import BeautifulSoup
+
+def generate_box(obj):
+    
+    xmin = int(obj.find('xmin').text)
+    ymin = int(obj.find('ymin').text)
+    xmax = int(obj.find('xmax').text)
+    ymax = int(obj.find('ymax').text)
+    
+    return [xmin, ymin, xmax, ymax]
+
+def generate_label(obj, classes: list):
+    for i in range(len(classes)):
+        if obj.find('name').text == classes[i]:
+            return i
+
+def generate_target(image_id, file, classes): 
+    with open(file) as f:
+        data = f.read()
+        soup = BeautifulSoup(data, 'xml')
+        objects = soup.find_all('object')
+
+        num_objs = len(objects)
+
+        # Bounding boxes for objects
+        # In coco format, bbox = [xmin, ymin, width, height]
+        # In pytorch, the input should be [xmin, ymin, xmax, ymax]
+        boxes = []
+        labels = []
+        for i in objects:
+            boxes.append(generate_box(i))
+            labels.append(generate_label(i, classes))
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # Labels (In my case, I only one class: target class or background)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        # Tensorise img_id
+        img_id = torch.tensor([image_id])
+        # Annotation is in dictionary format
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = img_id
+        
+        return target
+    
+class CustomDataset(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir("path to image folder")))
+        
+        # self.labels = list(sorted(os.listdir("/kaggle/input/face-mask-detection/annotations/")))
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        file_image = 'images'+ str(idx) + '.png'
+        file_label = 'images'+ str(idx) + '.xml'
+        img_path = os.path.join("path to image folder", file_image)
+        label_path = os.path.join("path to annotations", file_label)
+        img = Image.open(img_path).convert("RGB")
+        #Generate Label
+        target = generate_target(idx, label_path)
+        
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+    
+
